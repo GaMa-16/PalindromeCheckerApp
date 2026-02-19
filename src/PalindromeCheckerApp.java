a91b2f92d713c7193d0d70bb5fe2895c89ebc140
GaMa-16 <gn6521@srmist.edu.in>
2026-02-19 09:24:21 +0530

    UC1: Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,16 @@
 public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+        // UC1: Application Entry & Welcome Message
+
+        String appName = "Palindrome Checker";
+        String appVersion = "1.0.0";
+
+        System.out.println("********************************************");
+        System.out.println("      Welcome to " + appName);
+        System.out.println("      Version: " + appVersion);
+        System.out.println("********************************************");
+
+        // UC2 Logic will be added here later...
+    }
 }
